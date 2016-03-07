8864056b01f63fec04b551439e71585d2336e6a6
Kevin Stone <kevinastone@gmail.com>
2016-03-06 20:01:42 -0800

    Added flow types for the main entrypoint

--- interfaces/atom.js
+++ interfaces/atom.js
@@ -4,8 +4,22 @@ interface Atom$Packages {
   activatePackage(name: string): void;
 }
 
+type Atom$Grammar$Token = mixed;
+type Atom$Grammar$Rule = any;
+
+interface Atom$Grammar {
+  tokenizeLine(line: string, ruleStack: ?Array<Atom$Grammar$Rule>, firstLine: boolean)
+    : [Array<Atom$Grammar$Token>, Array<Atom$Grammar$Rule>];
+  tokenizeLines(text: string): Array<Atom$Grammar$Token>;
+}
+
+interface Atom$Grammars {
+  grammarForScopeName(name: string): Atom$Grammar;
+}
+
 type AtomGlobal = {
   packages: Atom$Packages;
+  grammars: Atom$Grammars;
 }
 
 declare var atom: AtomGlobal;
