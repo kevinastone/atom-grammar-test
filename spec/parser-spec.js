a64b89f594ef3498e4add65ead99399401f5e3e2
Kevin Stone <kevinastone@gmail.com>
2017-03-31 20:30:02 -0700

    Fixed lack of self in jasmine beforeEach

--- spec/parser-spec.js
+++ spec/parser-spec.js
@@ -6,50 +6,53 @@ import { lineFixture } from './utils';
 
 describe('AtomGrammarTest Parser', () => {
   describe('Header', () => {
+    const filename = 'blah.txt';
+    let parser;
+    let htmlParser;
+
     beforeEach(() => {
-      this.filename = 'blah.txt';
-      this.iterator = lineFixture(this.filename,
+      const iterator = lineFixture(filename,
         '// SYNTAX TEST "source.c"',
         '#pragma once',
         '// <- punctuation.definition.directive meta.preprocessor.c',
         ' // <- keyword.control.directive.pragma',
       );
-      this.parser = parse(this.iterator);
+      parser = parse(iterator);
     });
 
     beforeEach(() => {
-      this.htmlFilename = 'something.html';
-      this.htmlIterator = lineFixture(this.htmlFilename,
+      const htmlFilename = 'something.html';
+      const htmlIterator = lineFixture(htmlFilename,
         '<!-- SYNTAX TEST "text.html.basic" -->',
         '<TagName>',
         '<!-- <- punctuation.definition.tag.begin.html -->',
         '<!-- ^ entity.name.tag.other.html -->',
       );
-      this.htmlParser = parse(this.htmlIterator);
+      htmlParser = parse(htmlIterator);
     });
 
     it('should remember the filename', () => {
-      expect(this.parser.filename).toBe(this.filename);
+      expect(parser.filename).toBe(filename);
     });
 
     it('should parse the comment token', () => {
-      expect(this.parser.header.openToken).toBe('//');
-      expect(this.htmlParser.header.openToken).toBe('<!--');
-      expect(this.htmlParser.header.closeToken).toBe('-->');
+      expect(parser.header.openToken).toBe('//');
+      expect(htmlParser.header.openToken).toBe('<!--');
+      expect(htmlParser.header.closeToken).toBe('-->');
     });
 
     it('should parse the source declaration', () => {
-      expect(this.parser.header.scope).toBe('source.c');
-      expect(this.htmlParser.header.scope).toBe('text.html.basic');
+      expect(parser.header.scope).toBe('source.c');
+      expect(htmlParser.header.scope).toBe('text.html.basic');
     });
 
     it('should iterate the input lines', () => {
-      expect(Array.from(this.parser).map(line => line.line)).toEqual([
+      expect(Array.from(parser).map(line => line.line)).toEqual([
         '#pragma once',
         '// <- punctuation.definition.directive meta.preprocessor.c',
         ' // <- keyword.control.directive.pragma',
       ]);
-      expect(Array.from(this.htmlParser).map(line => line.line)).toEqual([
+      expect(Array.from(htmlParser).map(line => line.line)).toEqual([
         '<TagName>',
         '<!-- <- punctuation.definition.tag.begin.html -->',
         '<!-- ^ entity.name.tag.other.html -->',
